66209279927df547a5e8541bd1081f057a676322
Nick Robson <robson.nicholas1@gmail.com>
2020-04-10 16:14:57 +1000

    init music provider

--- server/src/main/java/com/github/langramming/model/MusicProviderType.java
+++ server/src/main/java/com/github/langramming/model/MusicProviderType.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.*;
 import com.github.langramming.model.spotify.SpotifyMusicProvider;
 import lombok.Getter;
 
+import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +15,19 @@ public enum MusicProviderType {
 
     @JsonValue
     private final String id;
-    private final Class<? extends MusicProvider> musicProvider;
+    private final MusicProvider musicProvider;
 
     MusicProviderType(String id, Class<? extends MusicProvider> musicProvider) {
         this.id = id;
-        this.musicProvider = musicProvider;
+
+        try {
+            Constructor<? extends MusicProvider> constructor = musicProvider.getConstructor();
+            constructor.setAccessible(true);
+            this.musicProvider = constructor.newInstance();
+        } catch (ReflectiveOperationException ignored) {
+            throw new IllegalStateException("Invalid music provider " + musicProvider.getName() + " - no no-args constructor.");
+        }
+
     }
 
     private static final Map<String, MusicProviderType> musicProviderTypeCache = new HashMap<>();
