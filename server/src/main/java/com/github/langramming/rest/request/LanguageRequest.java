37b56d6edf36d99ebaf5f6fe7cfaa6c702ce17a6
Nick Robson <robson.nicholas1@gmail.com>
2020-05-18 17:30:11 +1000

    fix language creation

--- server/src/main/java/com/github/langramming/rest/request/LanguageRequest.java
+++ server/src/main/java/com/github/langramming/rest/request/LanguageRequest.java
@@ -1,12 +1,20 @@
 package com.github.langramming.rest.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 import javax.annotation.Nonnull;
 
-@AllArgsConstructor(onConstructor_ = { @JsonCreator })
 public class LanguageRequest {
+
+    @JsonCreator
+    public LanguageRequest(
+            @JsonProperty("code") @Nonnull String code,
+            @JsonProperty("name") @Nonnull String name) {
+        this.code = code;
+        this.name = name;
+    }
 
     @Nonnull
     public String code;
