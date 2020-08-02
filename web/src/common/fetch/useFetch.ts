f137e3ab2ea557953b7940a8834c9aa81c3dd798
Nick Robson <robson.nicholas1@gmail.com>
2020-08-02 21:48:01 +1000

    use null instead of undefined

--- web/src/common/fetch/useFetch.ts
+++ web/src/common/fetch/useFetch.ts
@@ -8,19 +8,19 @@ type ActionType<T> =
 
 interface State<T> {
   loading: boolean;
-  data: T | undefined;
-  error: Error | undefined;
+  data: T | null;
+  error: Error | null;
 }
 
 const reducer = <T>(prevState: State<T>, action: ActionType<T>): State<T> => {
   if (action.type === "LOADING") {
-    return { loading: true, data: prevState.data, error: prevState.error };
+    return { ...prevState, loading: true };
   }
   if (action.type === "DATA") {
-    return { loading: false, data: action.data, error: undefined };
+    return { ...prevState, loading: false, data: action.data, error: null };
   }
   if (action.type === "ERROR") {
-    return { loading: false, data: undefined, error: action.error };
+    return { ...prevState, loading: false, error: action.error };
   }
   return prevState;
 };
@@ -32,8 +32,8 @@ export const useFetch = <T>(uri: string): State<T> => {
   ) => State<T>;
   const [{ loading, data, error }, dispatch] = React.useReducer(castReducer, {
     loading: false,
-    data: undefined,
-    error: undefined,
+    data: null,
+    error: null,
   });
 
   useEffect(() => {
