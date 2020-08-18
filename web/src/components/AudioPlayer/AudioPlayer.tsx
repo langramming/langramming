e78c298fa062f500bb1af25a2d986e816b831a76
Nick Robson <robson.nicholas1@gmail.com>
2020-08-18 15:51:48 +1000

    WIP - spotify track view

---
+++ web/src/components/AudioPlayer/AudioPlayer.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import ReactAudioPlayer from "react-audio-player";
+
+interface AudioPlayerProps {
+  src: string;
+}
+
+export const AudioPlayer = ({ src }: AudioPlayerProps) => {
+  const [audioRef, setAudioRef] = React.useState<HTMLAudioElement | null>(null);
+
+  const togglePlayPause = React.useCallback(() => {
+    if (audioRef == null) return;
+    if (audioRef.paused) {
+      audioRef.play();
+    } else {
+      audioRef.pause();
+    }
+  }, []);
+
+  return (
+    <>
+      <ReactAudioPlayer ref={setAudioRef} src={src} />
+      {audioRef && (
+        <button onClick={togglePlayPause}>
+          {audioRef.paused ? "Play" : "Pause"}
+        </button>
+      )}
+    </>
+  );
+};
--- web/src/components/SpotifyTrack/SpotifyTrack.tsx
+++ web/src/components/SpotifyTrack/SpotifyTrack.tsx
@@ -1,20 +1,39 @@
 import * as React from "react";
+import styled from "styled-components";
 import { Track } from "../../types/Track";
+import { AudioPlayer } from "../AudioPlayer";
 
 interface SpotifyTrackProps {
   track: Track;
   previewUrl: string;
 }
 
+const TrackContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-height: 192px;
+
+  > img {
+    max-height: inherit;
+    max-width: auto;
+  }
+`;
+
+const TrackDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 export const SpotifyTrack = ({ track, previewUrl }: SpotifyTrackProps) => (
-  <div>
-    <iframe
-      src={`https://open.spotify.com/embed/track/${track.id}`}
-      width="300"
-      height="80"
-      frameBorder="0"
-      allowTransparency
-      allow="encrypted-media"
+  <TrackContainer>
+    <img
+      src={track.images.sort((a, b) => b.width - a.width)[0].url}
+      alt={`Track album art for the track "${track.name}" from the album "${track.album.name}"`}
     />
-  </div>
+    <TrackDetailsContainer>
+      <h6>{track.name}</h6>
+      <p>{track.artists.map((artist) => artist.name).join(", ")}</p>
+      <AudioPlayer src={previewUrl} />
+    </TrackDetailsContainer>
+  </TrackContainer>
 );
