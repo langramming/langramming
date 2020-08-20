91eb1420e7d9211698750ccf2d66500bd2f6703e
Nick Robson <robson.nicholas1@gmail.com>
2020-08-20 20:08:05 +1000

    add logs to restart script

--- bin/restart-server.sh
+++ bin/restart-server.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
+set -e
 
 source ./creds.sh
 
@@ -6,10 +7,15 @@ mvn clean package
 
 if [ -e ./langramming.pid ]
 then
+  echo "Killing existing server instance..."
   kill "$(cat ./langramming.pid)" && rm ./langramming.pid
 fi
 
+echo "Migrating database..."
 mvn flyway:migrate -Dflyway.user="${DB_USER}" -Dflyway.password="${DB_PASSWORD}"
 
+echo "Starting server..."
 java -jar webapp/target/langramming-webapp-*.jar &
 echo $! > ./langramming.pid
+
+echo "Successfully started up server"
