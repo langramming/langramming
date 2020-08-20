045ef3e0193c3548a2778d9a583688fb9754f686
Nick Robson <robson.nicholas1@gmail.com>
2020-08-20 20:34:41 +1000

    save server output in logfiles

--- bin/restart-server.sh
+++ bin/restart-server.sh
@@ -3,20 +3,15 @@ set -e
 
 source "$(dirname $0)/creds.sh"
 
-echo "Building server..."
+set -x
 mvn clean package
 
 if [ -e ./langramming.pid ]
 then
-  echo "Killing existing server instance..."
   kill "$(cat ./langramming.pid)" && rm ./langramming.pid
 fi
 
-echo "Migrating database..."
 mvn flyway:migrate -Dflyway.user="${DB_USER}" -Dflyway.password="${DB_PASSWORD}"
 
-echo "Starting server..."
-java -jar webapp/target/langramming-webapp-*.jar &
+java -jar webapp/target/langramming-webapp-*.jar 2>>../error.log >>../access.log &
 echo $! > ./langramming.pid
-
-echo "Successfully started up server"
